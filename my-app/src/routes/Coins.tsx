194dd15e8776c7bc3233a03db7cd8e2ec971e005
mmsw1597 <mmsw1597@naver.com>
2022-08-22 07:51:56 +0900

    Create Coin Tracker App

---
+++ my-app/src/routes/Coins.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoins } from "../api";
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+    background-color: white;
+    color: ${props => props.theme.bgColor};
+    margin-bottom: 10px;
+    border-radius: 15px;
+    font-weight: 600;
+    a{
+        display: flex;
+        align-items:center;
+        padding: 20px;
+        
+    }
+    &:hover{
+        a{
+            color:${(props) => props.theme.accentColor};
+        }
+    }
+`;
+
+interface CoinInterface {
+    id: string;
+    name: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    symbol: string;
+}
+
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${props => props.theme.accentColor};
+`
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+    font-weight: 600;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
+`;
+
+function Coins() {
+    /*
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, []);
+    */
+    const { isLoading, data } = useQuery<CoinInterface[]>("allCoins", fetchCoins)
+
+    return (<Container>
+        <Header>
+            <Title>코인</Title>
+        </Header>
+        {isLoading ? (<Loader>Loading...</Loader>)
+            : (<CoinsList>
+                {data?.slice(0, 100).map(coin =>
+                    <Coin key={coin.id}>
+                        <Link to={{
+                            pathname: `/${coin.id}`,
+                            state: { name: coin.name },
+                        }} >
+                            <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                            {coin.name} &rarr;
+                        </Link>
+                    </Coin>)}
+            </CoinsList>)}
+    </Container>);
+}
+
+export default Coins;
---
+++ my-app/src/routes/Router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Coin from "./Coin";
+import Coins from "./Coins";
+
+function Router() {
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/:coinId">
+                <Coin />
+            </Route>
+            <Route path='/'>
+                <Coins />
+            </Route>
+        </Switch>
+    </BrowserRouter>
+}
+
+export default Router;
