7585be2398c73c62a679ebe4063fc02d8212b535
mmsw1597 <mmsw1597@naver.com>
2022-08-28 01:00:12 +0900

    Create Coin List Page

--- crypto-tracker/src/App.tsx
+++ crypto-tracker/src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import { createGlobalStyle } from "styled-components";
+import Router from "./routes/Router";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: ${props=>props.theme.bgColor};
+    color: ${props=>props.theme.textColor};
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  *{
+    box-sizing: border-box;
+  }
+`
 
 function App() {
   return (
-    <div>
-  
-    </div>
+    <>
+    <GlobalStyle/>
+    <Router/>
+    </>
   );
 }
 
